c8f2bcee9f18bb9dcb0ed983dfee657f5e20d2b4
Ирек <irek.haibulin@gmail.com>
2022-11-28 17:58:10 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Author testAuthor = new Author("Александр", "Грибоедов");
         Book book1 = new Book("Горе от ума", testAuthor, 1824);
         Author testAuthor2 = new Author("Иван", "Гончаров");
-        Book book2 = new Book("Обломов", testAuthor2, 1847);
+        Book book2 = new Book("Обломов", testAuthor2, 1846);
 
         book1.setReleaseYear(1825);
 
