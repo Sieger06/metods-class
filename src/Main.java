08123f6ba5c5dab961f33f1f9d2cdc67d0ca6601
Ирек <irek.haibulin@gmail.com>
2022-11-28 21:38:02 +0300

    Сделал дз

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Author testAuthor = new Author("Александр", "Грибоедов");
         Book book1 = new Book("Горе от ума", testAuthor, 1824);
         Author testAuthor2 = new Author("Иван", "Гончаров");
-        Book book2 = new Book("Обломов", testAuthor2, 1846);
+        Book book2 = new Book("Обломов", testAuthor2, 1845);
 
         book1.setReleaseYear(1825);
 
